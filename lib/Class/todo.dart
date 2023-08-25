f45c7e18bb56f673713ea1c263202393ecb2f1ce
Dishal Kanishka <87950341+Dotax14@users.noreply.github.com>
2023-08-25 13:51:43 +0530

    modified: 	modified:

---
+++ lib/Class/todo.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+//Declare Todo class attributes
+class Todo {
+  int id;
+  String title;
+  String description;
+  bool status;
+
+  Todo(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.status}) {
+    id = this.id;
+    title = this.title;
+    description = this.description;
+    status = this.status;
+  }
+
+  toJson() {
+    return {
+      "id": id,
+      "description": description,
+      "title": title,
+      "status": status
+    };
+  }
+
+  fromJson(jsonData) {
+    return Todo(
+        id: jsonData['id'],
+        title: jsonData['title'],
+        description: jsonData['description'],
+        status: jsonData['status']);
+  }
+}
--- lib/screens/to_do_body.dart
+++ lib/screens/to_do_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_application/Class/todo.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -8,11 +9,77 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List todos = [
+    Todo(id: 1, title: 'First To Do', description: "Test", status: false),
+    Todo(id: 2, title: 'Second To Do', description: "Test", status: false)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Text('data'),
-    );
+        backgroundColor: Colors.white,
+        body: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: todos.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 8.0,
+                margin:
+                    new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                child: InkWell(
+                  child: makeListTile(todos[index], index),
+                ),
+              );
+            }));
   }
+}
+
+makeListTile(Todo todo, index) {
+  return ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      leading: Container(
+        padding: const EdgeInsets.only(right: 12.0),
+        decoration: const BoxDecoration(
+            border: Border(right: BorderSide(width: 1.0, color: Colors.white))),
+        child: CircleAvatar(
+          backgroundColor: Color(0xFFECEFF1),
+          child: Text(
+            "${index + 1}",
+            style: TextStyle(color: Color(0xFF000000)),
+          ),
+        ),
+      ),
+      title: Row(
+        children: [
+          Text(
+            todo.title,
+            style: const TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          todo.status
+              ? const Icon(
+                  Icons.verified,
+                  color: Colors.greenAccent,
+                )
+              : Container()
+        ],
+      ),
+      // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+      subtitle: Wrap(
+        children: <Widget>[
+          Text(todo.description,
+              overflow: TextOverflow.clip,
+              maxLines: 1,
+              style: const TextStyle(color: Colors.black))
+        ],
+      ),
+      trailing: InkWell(
+          onTap: () {
+            // delete(todo);
+          },
+          child: Icon(Icons.remove, color: Colors.black, size: 20.0)));
 }
